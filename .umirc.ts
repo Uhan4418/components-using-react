ac36201ae539f1161c84e4e65772c333b280236b
yeyuxian <515235337@qq.com>
2022-03-21 11:49:01 +0800

    修改package.json

--- .umirc.ts
+++ .umirc.ts
@@ -28,7 +28,7 @@ export default defineConfig({
       skipNodeModules: true,
     },
   },
-  base: '/components-using-react/',
+  base: '/components-using-react/docs-dist/',
   publicPath: '/components-using-react/docs-dist/',
   history: {
     type: 'hash', // 设置路由模式为 hash 模式，防止部署至 GitHub Pages 后刷新网页后出现 404 的情况发生.
